33cca00f7b395d892367d5404dcd561ed218f3c0
MathavanN <math08avan@gmail.com>
2026-01-28 14:53:14 +0100

    Fix: Create a shared class library. ApiProbelmBuilder for NotFound, BadReques, Unauthorized added

---
+++ src/PlayProjectify.Core/ApiProblemBuilder/IApiProblemBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayProjectify.Core.ApiProblemBuilder;
+
+public interface IApiProblemBuilder
+{
+    ProblemDetails NotFound(string detail);
+    ProblemDetails BadRequest(string detail);
+    ProblemDetails Unauthorized(string detail);
+}
