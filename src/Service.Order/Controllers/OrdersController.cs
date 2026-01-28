b8592f21c1100073af969072a8936da946f80f38
MathavanN <math08avan@gmail.com>
2026-01-28 10:20:36 +0100

    Added sample controllers. Removed http files launch settings updated to run http profile app.UseHttpsRedirection() removed because they are going to deply behind the api gateway

---
+++ src/Service.Order/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Order.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok(new { Id = id, Total = 100 + id });
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(new[]
+        {
+        new { Id = 1, Total = 100 },
+        new { Id = 2, Total = 200 }
+    });
+    }
+}
---
+++ src/Service.Product/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Product.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok(new { Id = id, Name = $"Product {id}" });
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(
+            new[]
+            {
+                new { Id = 1, Name = "Product 1" },
+                new { Id = 2, Name = "Product 2" }
+            });
+    }
+}
