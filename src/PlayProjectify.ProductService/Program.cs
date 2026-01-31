2b6d90650087bed5ba7ecbc053603f43aee8a833
Mathavan <math08avan@gmail.com>
2026-01-31 16:28:38 +0100

    Fix: DI changes added

--- src/PlayProjectify.ProductService/Program.cs
+++ src/PlayProjectify.ProductService/Program.cs
@@ -1,4 +1,6 @@
 using PlayProjectify.ProductService.Apis;
+using PlayProjectify.ProductService.Data;
+using PlayProjectify.ProductService.Services;
 using PlayProjectify.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +10,16 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Services.AddProblemDetails();
 builder.AddApiVersioning();
+
+builder.Services.AddSingleton<ProductStore>();
+builder.Services.AddScoped<IProductService, InMemoryProductService>();
+
 string[] versions = ["v1", "v2"]; 
 builder.AddDefaultOpenApi(versions);
 
 var app = builder.Build();
 app.MapDefaultEndpoints();
 app.MapAboutApi();
+app.MapProductApi();
 app.UseDefaultOpenApi(versions);
 app.Run();
