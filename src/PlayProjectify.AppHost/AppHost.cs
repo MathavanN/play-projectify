d8fde4eff5654417f63619045dc946f64a0fd9f5
Mathavan <math08avan@gmail.com>
2026-02-04 16:34:15 +0100

    Fix: Keycloak added.

--- src/PlayProjectify.AppHost/AppHost.cs
+++ src/PlayProjectify.AppHost/AppHost.cs
@@ -1,14 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keycloak = builder.AddKeycloak("keycloak", 8080);
+
 var apiService = builder.AddProject<Projects.PlayProjectify_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+var productService = builder.AddProject<Projects.PlayProjectify_ProductService>("productservice")
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.PlayProjectify_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
     .WithReference(apiService)
-    .WaitFor(apiService);
-
-builder.AddProject<Projects.PlayProjectify_ProductService>("productservice");
+    .WithReference(productService)
+    .WaitFor(apiService)
+    .WaitFor(productService);
 
 builder.Build().Run();
