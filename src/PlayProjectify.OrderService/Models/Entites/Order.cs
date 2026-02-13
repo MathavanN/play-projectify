fb2feb6eb618f700ff884b0e68a4e905031542f0
Mathavan <math08avan@gmail.com>
2026-02-13 10:35:31 +0100

    Order Service model added

---
+++ src/PlayProjectify.OrderService/Models/Entites/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace PlayProjectify.OrderService.Models.Entites;
+
+public class Order : BaseEntity
+{
+    public Guid CustomerId { get; set; }
+    public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+    public decimal TotalAmount { get; set; }
+
+    public ICollection<OrderItem> Items { get; set; } = [];
+}
+
+public enum OrderStatus
+{
+    Pending,
+    Paid,
+    Shipped,
+    Cancelled
+}
---
+++ src/PlayProjectify.OrderService/Models/Entites/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace PlayProjectify.OrderService.Models.Entites;
+
+public class OrderItem : BaseEntity
+{
+    public Guid OrderId { get; set; }
+    public Order Order { get; set; } = null!;
+
+    // Reference only
+    public Guid ProductId { get; set; }
+
+    // Snapshot from Product service
+    public string ProductName { get; set; } = null!;
+    public decimal UnitPrice { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal TotalPrice => UnitPrice * Quantity;
+}
